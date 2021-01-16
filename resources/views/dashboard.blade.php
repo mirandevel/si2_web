57fd4e1484e6fbd3da6550ae11cad7236f7cc1b3
mirandevel <miranda.devel@gmail.com>
2021-01-15 21:06:28 -0400

    with role table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,22 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            <x-jet-dialog-modal>
+
+            </x-jet-dialog-modal>
         </h2>
     </x-slot>
+{{--            <div x-data="{ open: false }">
+                <button @click="open = true">Abrir Desplegable</button>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
+                <div
+                    x-show="open"
+                    @click.away="open = false"
+                >
+                    <x-menu >
+
+                    </x-menu>
+                </div>
+            </div>--}}
+
 </x-app-layout>
