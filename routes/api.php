eb2e3c4719315455ba0ee091632f3f61a9000978
mirandevel <miranda.devel@gmail.com>
2021-01-16 21:30:58 -0400

    api recover password

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\DatoMaestroController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +22,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/start');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate(['email' => 'required|email']);
+
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+
+    return $status === Password::RESET_LINK_SENT
+        ? ['status' => __($status)]
+        : ['email' => __($status)];
+})->middleware('guest');
+
+
+
+
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
@@ -29,3 +59,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/datalogin', [DatoMaestroController::class, 'datalogin']);
 
 Route::post('/store/token', [DatoMaestroController::class, 'storetoken']);
+
+
+
