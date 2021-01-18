684741a6e15b46d97d7cd6d672ea7a40f3a9ba30
mirandevel <miranda.devel@gmail.com>
2021-01-17 23:01:04 -0400

    repare login

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
             'password' => ['required'],
         ]);
         if($validator->fails()){
-            return response()->json(['error'=>$validator->messages()[0]]);
+            return response()->json(['error'=>$validator->messages()]);
         }
 
         $credentials=request(['email','password']);
