90c61f239bf4238e313c5718d69b02ee7b74a979
Oscar Alejandro Salazar <alejandrojesus666@gmail.com>
2021-02-02 10:15:01 -0400

    crud de categorias terminado

--- app/Http/Livewire/CategoriasTable.php
+++ app/Http/Livewire/CategoriasTable.php
@@ -11,7 +11,9 @@ class CategoriasTable extends Component
     use WithPagination;
 
     public $cantidadDeItemsPorPagina;
+    public $idDeCategoriaSeleccionada;
     public $nombre;
+    public $nombreDeCategoriaABuscar;
 
 //    protected $rules = [
 //        'nombre' => 'required|string|between:3,30',
@@ -20,6 +22,11 @@ class CategoriasTable extends Component
     public function mount()
     {
         $this->cantidadDeItemsPorPagina = 5;
+    }
+
+    public function resetarValores()
+    {
+        $this->reset(['nombre', 'idDeCategoriaSeleccionada',]);
     }
 
     public function updated($propertyName)
@@ -38,13 +45,40 @@ class CategoriasTable extends Component
         Categoria::create([
             'nombre' => $this->nombre
         ]);
-        $this->reset(['nombre',]);
+
+        $this->resetarValores();
+    }
+
+    public function cargarDatosDelFormularioEdit($id, $nombre)
+    {
+        $this->idDeCategoriaSeleccionada = $id;
+        $this->nombre = $nombre;
+    }
+
+    public function editCategoria()
+    {
+        $datosValidados = $this->validate([
+            'nombre' => 'required|string|between:3,30|unique:App\Models\Categoria,nombre',
+        ]);
+
+        $categoriaAEditar = Categoria::findOrFail($this->idDeCategoriaSeleccionada);
+        $categoriaAEditar->nombre = $this->nombre;
+        $categoriaAEditar->save();
+
+        $this->resetarValores();
+    }
+
+    public function eliminarCategoria()
+    {
+        Categoria::destroy($this->idDeCategoriaSeleccionada);
+        $this->resetarValores();
     }
 
     public function render()
     {
         return view('livewire.categorias-table', [
             'categorias' => Categoria::select('id', 'nombre')
+                ->where('categorias.nombre', 'LIKE', '%'.$this->nombreDeCategoriaABuscar.'%')
                 ->orderBy('categorias.id', 'asc')
                 ->paginate($this->cantidadDeItemsPorPagina)
         ]);
